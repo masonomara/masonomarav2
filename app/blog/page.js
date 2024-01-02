d56e3483e1ab8fd179ee5685eccb1ae46159ea2d
masonomara <mason.omara@gmail.com>
2024-01-02 04:28:03 -0500

    reorganized blog

--- app/blog/page.js
+++ app/blog/page.js
@@ -7,10 +7,13 @@ export const metadata = {
   title: "Blog",
 };
 
+const spaceId = process.env.CONTENTFUL_SPACE_ID;
+const contentfulAccessKey = process.env.CONTENTFUL_ACCESS_KEY;
+
 async function Blog() {
   const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    space: spaceId,
+    accessToken: contentfulAccessKey,
   });
   const res = await client.getEntries({ content_type: "bio" });
 
