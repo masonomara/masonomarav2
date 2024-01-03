6e77580dc04239a6812844a47d5be82ce76dbe8c
masonomara <mason.omara@gmail.com>
2024-01-03 13:24:28 -0500

    added 404 page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="notFoundWrapper">
+      <h1 className="titleMedium">Sorry Partner!</h1>
+      <p>404 Error: This page cannot be found.</p>
+    </div>
+  );
+}
